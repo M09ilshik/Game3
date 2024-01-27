c9bcccbde88e4e365607736915ae257b3402f578
M09ilshik <148627539+M09ilshik@users.noreply.github.com>
2024-01-27 17:04:55 +0500

    Add files via upload

--- Game/game.py
+++ Game/game.py
@@ -10,15 +10,17 @@ screen_hight = 720
 screen = pygame.display.set_mode((screen_width, screen_hight))
 
 # font = pygame.font.SysFont("Goudy Stout обычный", 50)
-# font2 = pygame.font.SysFont("TimesNewRoman", 30)
+font2 = pygame.font.SysFont("TimesNewRoman", 30)
 
-img = pygame.image.load("img/menu.jpg")
+score = 0
+
+img = pygame.image.load("img/menukirpichi.jpg")
 img_s = pygame.transform.scale(img, (1080, 720))
-img1 = pygame.image.load("img/zmeya.png")
+img1 = pygame.image.load("img/isaak-PhotoRoom.png-PhotoRoom.png")
 img1_s = pygame.transform.scale(img1, (100, 100))
-img2 = pygame.image.load("img/demon.png")
+img2 = pygame.image.load("img/antiisaak-PhotoRoom.png-PhotoRoom.png")
 img2_s = pygame.transform.scale(img2, (100, 100))
-img_bullet = pygame.image.load("img/bullet.png")
+img_bullet = pygame.image.load("img/bullet-PhotoRoom.png-PhotoRoom.png")
 img_bullet_s = pygame.transform.scale(img_bullet,(50,50))
 
 sound_stop = pygame.mixer.Sound("Sounds/vzryiv-yadernoy-bombyi.mp3")
@@ -35,7 +37,6 @@ flag_down = False
 flag_right = False
 flag_left = False
 flag_up = False
-
 def hero():
     global img_x
     global img_y
@@ -50,25 +51,25 @@ def hero():
         img_x += speed
     screen.blit(img1_s, (img_x, img_y))
 
-# def antihero():
-#     global img_x1
-#     global img_y1
-#
-#     if img_y1 + 100 >= 620:
-#         img_y1 -= speed2
-#         sound_antihero.play()
-#     elif img_y1 <= 0:
-#         img_y1 += speed2
-#         sound_antihero.play()
-#     else:
-#         sound_antihero.stop()
-#     screen.blit(img2_s, (img_x1, img_y1))
+def antihero():
+    global img_x1
+    global img_y1
 
-# def bullet():
-#     global img_bullet_x
-#     global img_bullet_y
-#
-#     i
+    if img_y + 70 < img_y1:
+        img_y1 -= speed2
+        sound_antihero.play()
+    elif img_y > img_y1 + 70:
+        img_y1 += speed2
+        sound_antihero.play()
+    elif img_x + 70 < img_x1:
+        img_x1 -= speed2
+        sound_antihero.play()
+    elif img_x > img_x1 + 70:
+        img_x1 += speed2
+        sound_antihero.play()
+    else:
+        sound_antihero.stop()
+    screen.blit(img2_s, (img_x1, img_y1))
 
 def bullet():
     global x_bull
@@ -94,18 +95,30 @@ def bullet():
         flag_up = False
 
     if flag_right == True:
+        flag_left = False
+        flag_up = False
+        flag_down = False
         x_bull += 1
         screen.blit(img_bullet_s, (x_bull, y_bull))
-    elif flag_left == True:
+    if flag_left == True:
+        flag_right = False
+        flag_up = False
+        flag_down = False
         x_bull -= 1
         screen.blit(img_bullet_s, (x_bull, y_bull))
-    elif flag_up == True:
+    if flag_up == True:
+        flag_right = False
+        flag_left = False
+        flag_down = False
         y_bull -= 1
         screen.blit(img_bullet_s, (x_bull, y_bull))
-    elif flag_down == True:
+    if flag_down == True:
+        flag_right = False
+        flag_up = False
+        flag_left = False
         y_bull += 1
         screen.blit(img_bullet_s, (x_bull, y_bull))
-    else:
+    if flag_right != True and flag_left != True and flag_up != True and flag_down != True:
         x_bull = img_x
         y_bull = img_y
 
@@ -120,12 +133,33 @@ while True:
 
     hero()
     bullet()
-    # antihero()
+    antihero()
     
-    # if img_x + 100  > img_x1 and img_x < img_x1 + 100 and img_y + 100 > img_y1 and img_y < img_y1 + 100:
-    #     speed = 0
-    #     speed2 = 0
-    #     sound_stop.play()
+    if img_x1 < img_x + 71 and img_x1 + 71 > img_x and img_y1 < img_y + 71 and img_y1 + 71 > img_y:
+        speed = 0
+        speed2 = 0
+        print(1)
+        sound_stop.play()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+    if img_x1 < x_bull + 70 and img_x1 + 70 > x_bull and img_y1 < y_bull + 70 and img_y1 + 70 > y_bull:
+        score += 1
+        antihero_spawn = random.randint(300,500)
+        antihero_spawn2 = random.randint(1,4)
+        if antihero_spawn2 == 1:
+            img_x1 = img_x - antihero_spawn
+        if antihero_spawn2 == 2:
+            img_x1 = img_x + 100 + antihero_spawn
+        if antihero_spawn2 == 3:
+            img_y1 = img_y - antihero_spawn
+        if antihero_spawn2 == 4:
+            img_y1 = img_y + 100 + antihero_spawn
+
+
 
     if img_x + 100 > 1080:
         img_x = 1
@@ -137,8 +171,8 @@ while True:
         img_y = 619
 
         
-    # text = font2.render("Start", True, (0,0,225))
-    # screen.blit(text, (500, 320))
+    # text = font2.render(score, True, (0,0,225))
+    # screen.blit(text, (0, 0))
     # text = font.render("Welcome", True, (0, 255, 0))
     # screen.blit(text, (455, 200))
     # text = font2.render("Options", True, (0,0,255))
